4daa37d26c649bbfe113671751e0613d456da585
bh20511 <bh20511@gmail.com>
2023-01-29 14:08:25 +0800

    修正linepay中

--- routes/cart.js
+++ routes/cart.js
@@ -20,24 +20,6 @@ const {
 } = process.env;
 
 
-//輸入uuid跟 指定的linebody 他會幫你轉成可以送給line的headers
-function createSignature(orders_num, linePayBody) {
-  const nonce = orders_num;
-  const string = `${LINEPAY_CHANNEL_SECRET_KEY}/${LINEPAY_VERSION}${uri}${JSON.stringify(
-    linePayBody
-  )}${nonce}`;
-  //製作簽章
-  const signature = Base64.stringify(
-    HmacSHA256(string, LINEPAY_CHANNEL_SECRET_KEY)
-  );
-  const headers = {
-    "Content-Type": "application/json",
-    "X-LINE-ChannelId": LINEPAY_CHANNEL_ID,
-    "X-LINE-Authorization-Nonce	": nonce,
-    "X-LINE-Authorization	": signature,
-  };
-  return headers;
-}
 
 // 下面是把訂單寫入資料庫
 async function createOrders(req, res) {
@@ -45,40 +27,38 @@ async function createOrders(req, res) {
     success: false,
     error: "",
     postData: req.body, //除錯用
-    auth: {},
   };
   //取出買什麼東西  會員編號 還有付款方式
   const { orders, member_sid, payWay } = req.body;
-  console.log(orders);
   //製作訂單的uuid編號
   const orders_num = uuidv4();
   //要用for迴圈跑出總價 先製作一個變數
   let totalPrice = 0;
-  // console.log(orders);
 
-  //把每一個品項寫入子訂單
+  //把每一個品項先查好金額並寫好物件
   for (let i = 0; i < orders.length; i++) {
     const pSql = `SELECT price,name FROM products WHERE sid = ${orders[i].sid}`;
-    // console.log(orders[i.sid]);
     const [result] = await db.query(pSql);
-    // console.log(result);
     orders[i].price = +result[0].price;
     orders[i].name = result[0].name;
     //把商品細項的部分透過資料庫查好
+    //經過上面之後 購物車orders陣列中的每個物件 都會有 {sid,amount,price,name} 這些屬性 
 
+    //接下來要寫入子訂單的有  訂單編號,商品編號,商品數量,品項購買金額
     const oSql =
       "INSERT INTO order_details(orders_num,product_sid,amount,subtotal) VALUES (?,?,?,?)";
 
     const detailsResult = await db.query(oSql, [
-      orders_num, //uuid特別編號
-      orders[i].sid, //哪個會員買的
-      orders[i].amount, //數量
-      orders[i].price * orders[i].amount, //品項金額
+      orders_num,                                             //uuid訂單編號
+      orders[i].sid,                                          //商品編號
+      orders[i].amount,                                       //數量
+      orders[i].price * orders[i].amount,                     //品項金額
     ]);
-    totalPrice += orders[i].price * orders[i].amount; //計算總金額
+    totalPrice += orders[i].price * orders[i].amount;         //計算總金額  !!這邊是要給母訂單寫入的  子訂單沒用到!!
   }
 
   //寫入母訂單,同時付款狀態要記得先寫未付款
+  //要寫的資料分別有  訂單編號(uuid),會員編號,總金額,付款方式,付款狀態(未付款),訂單成立時間(NOW)
   const sql =
     "INSERT INTO orders(orders_num,member_sid,total_price,pay_way,pay_state,ordered_at) VALUEs(?,?,?,?,?,NOW())";
   const [result2] = await db.query(sql, [
@@ -89,42 +69,100 @@ async function createOrders(req, res) {
     "未付款",
   ]);
 
+
+
+
+  //輸入uuid跟 指定的linebody 他會幫你轉成可以送給line的headers
+  // function createSignature(orders_num, linePayBody) {
+  //   const nonce = orders_num;
+  //   const string = `${LINEPAY_CHANNEL_SECRET_KEY}/${LINEPAY_VERSION}${uri}${JSON.stringify(
+  //     linePayBody
+  //   )}${nonce}`;
+  //   //製作簽章
+  //   const signature = Base64.stringify(
+  //     HmacSHA256(string, LINEPAY_CHANNEL_SECRET_KEY)
+  //   );
+  //   const headers = {
+  //     "Content-Type": "application/json",
+  //     "X-LINE-ChannelId": LINEPAY_CHANNEL_ID,
+  //     "X-LINE-Authorization-Nonce	": nonce,
+  //     "X-LINE-Authorization	": signature,
+  //   };
+  //   return headers;
+  // }
+
+  function createSignature(linePayBody) {
+    const nonce = orders_num
+
+    const encrypt = HmacSHA256(`${LINEPAY_CHANNEL_SECRET_KEY}/${LINEPAY_VERSION}${uri}${JSON.stringify(linePayBody)}${nonce}`, LINEPAY_CHANNEL_SECRET_KEY)
+
+    const signature = Base64.stringify(encrypt);
+    // console.log('test:', signature);
+
+    const headers = {
+      'Content-Type': 'application/json',
+      'X-LINE-ChannelId': LINEPAY_CHANNEL_ID,
+      'X-LINE-Authorization-Nonce': nonce,
+      'X-LINE-Authorization': signature,
+    };
+    return headers;
+  }
+
+
+
+  //為什麼我這邊要寫新陣列? 看起來我是想要紀錄商品編號,商品名稱,商品數量,商品價格
   const newOrder = orders.map((e, i) => {
-    return { sid: e.sid, name: e.name, quantity: e.amount, price: e.price };
+    return { name: e.name, quantity: e.amount, price: e.price };
   });
 
-  console.log(newOrder);
+  // console.log(newOrder);
+
+
+
+  //   function createLinePayBody(order) {
+  //     return {
+  //         ...order,
+  //         currency: 'TWD',
+  //         redirectUrls: {
+  //             confirmUrl: `${LINEPAY_RETURN_HOST}${LINEPAY_RETURN_CONFIRM_URL}`,
+  //             cancelUrl: `${LINEPAY_RETURN_HOST}${LINEPAY_RETURN_CANCEL_URL}`,
+  //           },
+  //     }
+  // }
+
 
   const linePayBody = {
-    1: {
-      amount: totalPrice,
-      currency: "TWD",
-      redirectUrls: {
-        confirmUrl: `${LINEPAY_RETURN_HOST}/${LINEPAY_RETURN_CONFIRM_URL}`,
-        cancelUrl: `${LINEPAY_RETURN_HOST}/${LINEPAY_RETURN_CANCEL_URL}`,
-      },
-      packages: [
-        {
-          id: "1",
-          amount: totalPrice,
-          products: [...newOrder],
-        },
-      ],
+    amount: totalPrice,
+    currency: "TWD",
+    orderId: orders_num,
+    redirectUrls: {
+      confirmUrl: `${LINEPAY_RETURN_HOST}/${LINEPAY_RETURN_CONFIRM_URL}`,
+      cancelUrl: `${LINEPAY_RETURN_HOST}/${LINEPAY_RETURN_CANCEL_URL}`,
     },
+    packages: [
+      {
+        id: "1",
+        amount: totalPrice,
+        products: newOrder,
+      },
+    ],
   };
-  // console.log("helloooooo", linePayBody[1].packages[0].products);
-  const headers = createSignature(orders_num, linePayBody);
 
+  const headers = createSignature(linePayBody);
   const url = `${LINEPAY_SITE}/${LINEPAY_VERSION}${uri}`;
-  const linePayRes = await axios.post(url, linePayBody, { headers });
-  console.log(linePayRes.data);
-
-
-  
-  if (result2.affectedRows) {
+  const {data} = await axios.post(url, linePayBody, { headers });
+  console.log(data);
+  if(data.returnCode==='0000'){
     output.success = true;
+    output.url = data.info.paymentUrl.web
+    output.orders_num = orders_num
+  }else{
+    output.error = '訂單失敗'
   }
-  return { output, orders_num };
+  // if (result2.affectedRows) {
+  //   output.success = true;
+  // }
+  return { output };
 }
 
 router.post("/createOrders", async (req, res) => {
