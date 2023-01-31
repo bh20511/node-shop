3a24b62b79e38801d71aabf6135d7f1a103d156f
bh20511 <bh20511@gmail.com>
2023-02-01 01:48:31 +0800

    to愉安

--- routes/product.js
+++ routes/product.js
@@ -3,31 +3,26 @@ const router = express.Router();
 const db = require(__dirname + "/../modules/db_connect.js");
 
 async function getList(req) {
-  // console.log('helloooooooooooooooo')
   const perPage = 20;
   let page = +req.query.page || 1;
   let where = `WHERE 1`
 
   //判斷有沒有種類條件
   let category = +req.query.category ? req.query.category.trim() : "";
-  // console.log(category)
   if (category) {
     where += ` AND c.parent_sid = '${category}'`
   }
   //再來處理有幾筆 要弄幾個分頁的問題
   const sql1 = `SELECT COUNT(1) count FROM products p JOIN product_categories c ON c.sid= p.category ${where}`
   const [[{ count }]] = await db.query(sql1)
-  console.log(count)
   let totalPages = 0;
   if(count > 0) {
     totalPages = Math.ceil(count / perPage);
   }
-  console.log(totalPages)
+  
   //開始處理每頁問題
-  // const sql = `SELECT p.* FROM products p JOIN product_categories c ON c.sid = p.category ${where}`
   const sql = `SELECT p.* FROM products p JOIN product_categories c ON c.sid = p.category ${where} LIMIT ${(page - 1) * perPage},${perPage}`
-
-  const [result] = await db.query(sql)
+const [result] = await db.query(sql)
   return { result , count , totalPages }
 }
 
